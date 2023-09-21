85c374d3da983b096484e8307f92546416d562be
shaheen2013 <coredeveloper.2013@gmail.com>
2023-09-21 22:24:22 +0600

    added readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project setup
+
+* Extract the zip file
+* Rename .env.example to .env
+* Add open weather api key and jwt token on .env file. 
+* run `docker-compose -f docker-compose.dev.yml up -d` for development or run `docker-compose -f docker-compose.prod.yeml up -d` for production environment
+* user username `admin` and password `password` for login
+  
--- test/auth.test.js
+++ test/auth.test.js
@@ -1,12 +1,11 @@
 const request = require('supertest');
 const expect = require('chai').expect;
-const app = require('../server');
+const app = require('../app');
 
 describe('Weather API', () => {
     it('should authenticate user', async () => {
-        const res = await request(app).post('/login').send({ username: 'admin', password: 'password123' });
-        expect(res.statusCode).toEqual(200);
+        const res = await request(app).post('/login').send({ username: 'admin', password: 'password' });
+        expect(res.statusCode).equal(200);
         expect(res.body).to.have.property('token');
     });
-    // ... other tests
 });
